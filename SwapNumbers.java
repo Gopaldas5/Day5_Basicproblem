949bd0b5ddbe49709b10c2e1d1748d2fa490fdce
GopalDas <gopalkrdas1997@gmail.com>
2022-05-24 06:25:53 +0530

    Swapping the two number

---
+++ SwapNumbers.java
@@ -0,0 +1,23 @@
+package com.bridgeLabz.Day5_BasiccoreProblems;
+
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(".....Before the Swapping.....");
+        System.out.print("Enter 1st number:- ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter 2nd number:- ");
+        int num2 = scanner.nextInt();
+        int temp;
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println(".....After The Swapping.....");
+        System.out.println("Enter 1st number:- "+num1);
+        System.out.println("Enter 2nd number:- "+num2);
+
+
+    }
+}
