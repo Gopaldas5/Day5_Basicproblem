9a2bd9972e10ff12a38904304e44a2cdb8eff793
GopalDas <gopalkrdas1997@gmail.com>
2022-05-24 06:01:58 +0530

    Find the year is leap year or not

---
+++ LeapYear.java
@@ -0,0 +1,17 @@
+package com.bridgeLabz.Day5_Basiccore;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year;
+        System.out.println("Enter an Year:- ");
+        Scanner scanner = new Scanner(System.in);
+        year = scanner.nextInt();
+
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+            System.out.println("This is a leap year:- ");
+        else
+            System.out.println("This is not a leap year:- ");
+    }
+}
