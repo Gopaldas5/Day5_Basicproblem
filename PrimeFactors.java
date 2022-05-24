27f0d0a91573388234c85de6152f1a2810102bdf
GopalDas <gopalkrdas1997@gmail.com>
2022-05-24 06:21:13 +0530

    checking the prime factor number

---
+++ PrimeFactors.java
@@ -0,0 +1,27 @@
+package com.bridgeLabz.Day5_BasiccoreProblems;
+
+import java.util.Scanner;
+
+public class PrimeFactors {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the number:- ");
+        int n = scn.nextInt();
+        int num;
+
+        for ( num = 2; num * num <= n; num++)
+        {
+            while (n % num == 0)
+            {
+                System.out.print(num + " ");
+                n = n / num;
+            }
+        }
+        if (n != 1)
+        {
+            System.out.print(n);
+        }
+
+    }
+}
