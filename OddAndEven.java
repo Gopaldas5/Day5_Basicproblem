551111f61a3f468a226fc89cef873fea8c3387b6
Gopal kumar Das <35631909+Gopaldas5@users.noreply.github.com>
2022-07-04 08:38:39 +0530

    Update OddAndEven.java

--- OddAndEven.java
+++ OddAndEven.java
@@ -6,12 +6,12 @@ public class OddAndEven {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number :- ");
         int num = scanner.nextInt();
 
         if(num % 2 == 0)
-            System.out.println( "It is even number:- "+num);
+            System.out.println( "It is even number :- "+num);
         else
-            System.out.println("It is odd number:- "+num);
+            System.out.println("It is odd number :- "+num);
     }
 }
