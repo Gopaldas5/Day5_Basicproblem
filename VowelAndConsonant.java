c0d951390c59e7b4d0611139ceea41259af1f90b
Gopal kumar Das <35631909+Gopaldas5@users.noreply.github.com>
2022-07-04 08:49:06 +0530

    Update VowelAndConsonant.java

--- VowelAndConsonant.java
+++ VowelAndConsonant.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class VowelAndConsonant {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the Alphabet:- ");
+        System.out.print("Enter the Alphabet :- ");
         char alp = scanner.next().charAt(0);
 
         if(alp == 'a' || alp == 'e' || alp == 'i' || alp == 'o'
                 || alp == 'u' || alp == 'A' || alp == 'E' || alp == 'I'
                 || alp == 'O' || alp == 'U')
-            System.out.println("  It is vowel:- " +alp);
+            System.out.println("  It is vowel :- " +alp);
         else
-            System.out.println( "It is consonant:- "+alp);
+            System.out.println( "It is consonant :- "+alp);
 
     }
 }
