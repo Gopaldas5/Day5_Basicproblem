3e9b17c155d1cbeb268bf6c23e29bd002cc0bc5a
Gopal kumar Das <35631909+Gopaldas5@users.noreply.github.com>
2022-07-04 08:44:59 +0530

    Update SwapNumbers.java

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -6,17 +6,17 @@ public class SwapNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(".....Before the Swapping.....");
-        System.out.print("Enter 1st number:- ");
+        System.out.print("Enter 1st number :- ");
         int num1 = scanner.nextInt();
-        System.out.print("Enter 2nd number:- ");
+        System.out.print("Enter 2nd number :- ");
         int num2 = scanner.nextInt();
         int temp;
         temp = num1;
         num1 = num2;
         num2 = temp;
         System.out.println(".....After The Swapping.....");
-        System.out.println("Enter 1st number:- "+num1);
-        System.out.println("Enter 2nd number:- "+num2);
+        System.out.println("Enter 1st number :- "+num1);
+        System.out.println("Enter 2nd number :- "+num2);
 
 
     }
