29701dbe528c0bc5a1614d5206322912074d454a
Gopal kumar Das <35631909+Gopaldas5@users.noreply.github.com>
2022-07-04 08:29:55 +0530

    Update Flipcoin.java

--- Flipcoin.java
+++ Flipcoin.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Flipcoin{
     public static void main(String[] args) {
 
-        System.out.println("Enter number of times to flip the coin");
+        System.out.println("Enter number of times to flip the coin:- ");
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         int loopCount = 0;
@@ -21,10 +21,10 @@ public class Flipcoin{
                 System.out.println("The result is tails.");
              loopCount ++;
         }
-        double percentHeads = heads/number*100;
-        double percentTails = (number-heads)/number*100;
-        System.out.println("The percentage of heads is: " +percentHeads);
-        System.out.println("The percentage of tails is: " +percentTails);
+        double percentHeads = heads / number*100;
+        double percentTails = (number-heads) / number*100;
+        System.out.println("The percentage of heads is:- " +percentHeads);
+        System.out.println("The percentage of tails is:- " +percentTails);
     }
 }
 
